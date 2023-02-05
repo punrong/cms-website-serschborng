64d73ab39e5a69d0ad048d2032abe22f0ba998e4
Punrong <punrongrany1999@gmail.com>
2023-02-05 01:10:55 -0500

    feat(Login): allow only ACT status users who can log in

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
+use App\Models\User;
 
 class LoginRequest extends FormRequest
 {
@@ -39,17 +40,24 @@ class LoginRequest extends FormRequest
      */
     public function authenticate(): void
     {
-        $this->ensureIsNotRateLimited();
+        $userStatus = User::where('email', $this->input('email'))->value('status');
+        if($userStatus == 'ACT'){
+            $this->ensureIsNotRateLimited();
 
-        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
-            RateLimiter::hit($this->throttleKey());
-
+            if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+                RateLimiter::hit($this->throttleKey());
+    
+                throw ValidationException::withMessages([
+                    'email' => trans('auth.failed'),
+                ]);
+            }
+    
+            RateLimiter::clear($this->throttleKey());
+        } else {
             throw ValidationException::withMessages([
                 'email' => trans('auth.failed'),
-            ]);
+            ]); 
         }
-
-        RateLimiter::clear($this->throttleKey());
     }
 
     /**
