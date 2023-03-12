c3302f7290f8a5fd2268067bcb255405986a0e18
Punrong <punrongrany1999@gmail.com>
2023-03-11 21:38:13 -0500

    feat(CKEditor): add ckeditor class build

---
+++ resources/js/components/Editor.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <ckeditor
+            :editor="editor"
+            v-model="editorData"
+            :content="content"
+            @input="onChange"
+        ></ckeditor>
+    </div>
+</template>
+<script>
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import CKEditor from "@ckeditor/ckeditor5-vue";
+import "ckeditor-tailwind-reset/ckeditor-tailwind-reset.css";
+
+export default {
+    components: {
+        ckeditor: CKEditor.component,
+    },
+    props: {
+        content: {
+            type: String,
+            default: () => "",
+        },
+    },
+    data() {
+        return {
+            editor: ClassicEditor,
+            editorData: this.content,
+        };
+    },
+    methods: {
+        onChange(data) {
+            this.$emit("updateEditorData", data);
+        },
+    },
+};
+</script>
+<style>
+.ck-editor__editable_inline {
+    min-height: 300px !important;
+}
+</style>
