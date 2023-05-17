b6c15d985e65e24814cfaa2d4bc9688ccfd805dc
Punrong <punrongrany1999@gmail.com>
2023-05-17 17:44:31 -0400

    style(Register): improve UI

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -20,7 +20,15 @@ const onSubmit = () => {
 <template>
     <GuestLayout>
         <Head title="Register" />
+        <div class="mb-2 text-center">
+            <header>
+                <h2 class="text-2xl font-bold text-gray-900">REGISTER</h2>
 
+                <p class="text-sm text-gray-600">
+                    very excited to have you with us
+                </p>
+            </header>
+        </div>
         <FormKit
             type="form"
             @submit="onSubmit"
@@ -88,7 +96,7 @@ const onSubmit = () => {
             <div class="flex items-center justify-end mt-4">
                 <Link
                     :href="route('login')"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    class="underline text-sm text-blue-600 hover:text-blue-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                     Already registered?
                 </Link>
@@ -99,7 +107,7 @@ const onSubmit = () => {
                     :disabled="form.processing"
                     :classes="{
                         outer: form.processing ? 'm-0 text-right ml-4 opacity-25' : 'm-0 text-right ml-4',
-                        input: 'bg-blue-500 text-white font-bold px-3 w-auto mb-2 ',
+                        input: 'bg-blue-500 hover:bg-blue-800 text-white font-bold px-3 w-auto mb-2 ',
                     }"
                 />
             </div>
