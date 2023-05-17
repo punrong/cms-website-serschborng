ec94a90fa796588a94e72bb5f440c74f72a545d5
Punrong <punrongrany1999@gmail.com>
2023-05-17 00:27:01 -0400

    feat(web): add logo
    - add logo to display it in broswer tab

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="icon" href="/logo.png" type="image/png">
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
