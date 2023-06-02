b622c42e9cb39abb87cb823397025f5a7e4cc115
Punrong <punrongrany1999@gmail.com>
2023-06-02 17:06:16 -0400

    style(Features, Mission & Services): remove bent lines

--- resources/js/Pages/Front/components/Features.vue
+++ resources/js/Pages/Front/components/Features.vue
@@ -87,18 +87,6 @@
                             class="w-full align-middle rounded-t-lg"
                         />
                         <blockquote class="relative p-8 mb-4">
-                            <svg
-                                preserveAspectRatio="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 583 95"
-                                class="absolute left-0 w-full block"
-                                style="height: 95px; top: -94px"
-                            >
-                                <polygon
-                                    points="-30,95 583,95 583,65"
-                                    class="fill-current text-blue-600"
-                                ></polygon>
-                            </svg>
                             <h4 class="text-xl font-bold text-white">
                                 {{ aboutUsCardData.title }}
                             </h4>
