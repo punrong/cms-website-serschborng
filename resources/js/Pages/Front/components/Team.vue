a99d0cfc65174ab7181369a4836ed377d57503de
Punrong <punrongrany1999@gmail.com>
2023-06-02 17:59:24 -0400

    style(Team): remove padding from image and title of member section

--- resources/js/Pages/Front/components/Team.vue
+++ resources/js/Pages/Front/components/Team.vue
@@ -3,25 +3,29 @@
         <div class="container mx-auto px-4">
             <div class="flex flex-wrap justify-center text-center mb-10">
                 <div class="w-full lg:w-6/12 px-4">
-                    <h2 class="text-4xl font-semibold">{{ ourHeroTitleData.title }}</h2>
-                    <p class="text-lg leading-relaxed m-4 text-gray-600" v-html="ourHeroTitleData.description">
-                    </p>
+                    <h2 class="text-4xl font-semibold">
+                        {{ ourHeroTitleData.title }}
+                    </h2>
+                    <p
+                        class="text-lg leading-relaxed m-4 text-gray-600"
+                        v-html="ourHeroTitleData.description"
+                    ></p>
                 </div>
             </div>
             <div class="flex flex-wrap justify-center">
                 <div class="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
-                    <div class="px-6">
-                        <img
-                            :src="ourHeroItemData.image"
-                            class="shadow-lg rounded-full max-w-full mx-auto"
-                        />
-                        <div class="pt-6 text-center">
-                            <h5 class="text-xl font-bold">{{ ourHeroItemData.title }}</h5>
-                            <p
-                                class="mt-1 text-sm text-gray-500 uppercase font-semibold" v-html="ourHeroItemData.description"
-                            >
-                            </p>
-                        </div>
+                    <img
+                        :src="ourHeroItemData.image"
+                        class="shadow-lg rounded-full max-w-full mx-auto"
+                    />
+                    <div class="pt-6 text-center">
+                        <h5 class="text-xl font-bold">
+                            {{ ourHeroItemData.title }}
+                        </h5>
+                        <p
+                            class="mt-1 text-sm text-gray-500 uppercase font-semibold"
+                            v-html="ourHeroItemData.description"
+                        ></p>
                     </div>
                 </div>
             </div>
