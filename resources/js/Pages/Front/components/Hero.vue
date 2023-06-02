2cad097545b08c645657f4436b9617faf4d2f6dc
Punrong <punrongrany1999@gmail.com>
2023-06-02 17:47:39 -0400

    style(Hero): remove bent lines

--- resources/js/Pages/Front/components/Hero.vue
+++ resources/js/Pages/Front/components/Hero.vue
@@ -21,30 +21,13 @@
                         <h1 class="text-white font-semibold text-5xl">
                             {{ coverData.title }}
                         </h1>
-                        <p class="mt-4 text-lg text-gray-300" v-html="coverData.description">
-                        </p>
+                        <p
+                            class="mt-4 text-lg text-gray-300"
+                            v-html="coverData.description"
+                        ></p>
                     </div>
                 </div>
             </div>
-        </div>
-        <div
-            class="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden"
-            style="height: 70px; transform: translateZ(0px)"
-        >
-            <svg
-                class="absolute bottom-0 overflow-hidden"
-                xmlns="http://www.w3.org/2000/svg"
-                preserveAspectRatio="none"
-                version="1.1"
-                viewBox="0 0 2560 100"
-                x="0"
-                y="0"
-            >
-                <polygon
-                    class="text-gray-300 fill-current"
-                    points="2560 0 2560 100 0 100"
-                ></polygon>
-            </svg>
         </div>
     </div>
 </template>
