1a2f3951bc113d186c1c926cb7c1723bd652e247
Punrong <punrongrany1999@gmail.com>
2023-06-20 15:07:41 -0400

    feat(Frontend): remove black bg from cover
    a couple of updates:
    - apply that to the mentor detail
    - apply that to the opportunity detail
    - apply that to the blog detail

--- resources/js/Pages/Front/Mentorship/components/Carousel.vue
+++ resources/js/Pages/Front/Mentorship/components/Carousel.vue
@@ -7,23 +7,7 @@
             class="absolute top-0 w-full h-full bg-center bg-cover"
             :style="`background-image: url('${mentor.image}')`"
         >
-            <span
-                id="blackOverlay"
-                class="w-full h-full absolute opacity-75 bg-black"
-            ></span>
-        </div>
-        <div class="container relative mx-auto">
-            <div class="items-center flex flex-wrap">
-                <div
-                    class="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center mt-9 sm:mt-0"
-                >
-                    <div>
-                        <h1 class="text-white font-semibold text-5xl">
-                            {{ mentor.name }}
-                        </h1>
-                    </div>
-                </div>
-            </div>
+            <span id="blackOverlay" class="w-full h-full absolute"></span>
         </div>
     </div>
 </template>
--- resources/js/Pages/Front/Opportunities/components/Carousel.vue
+++ resources/js/Pages/Front/Opportunities/components/Carousel.vue
@@ -7,29 +7,7 @@
             class="absolute top-0 w-full h-full bg-center bg-cover"
             :style="`background-image: url('${opportunity.image}')`"
         >
-            <span
-                id="blackOverlay"
-                class="w-full h-full absolute opacity-75 bg-black"
-            ></span>
-        </div>
-        <div class="container relative mx-auto">
-            <div class="items-center flex flex-wrap">
-                <div
-                    class="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center mt-9 sm:mt-0"
-                >
-                    <div>
-                        <h1 class="text-white font-semibold text-5xl">
-                            {{ opportunity.title }}
-                        </h1>
-                        <p class="mt-4 text-lg text-gray-300">
-                            Author: {{ opportunity.publisher }}
-                        </p>
-                        <p class="text-lg text-gray-300">
-                            Published Date: {{ opportunity.publish_date }}
-                        </p>
-                    </div>
-                </div>
-            </div>
+            <span id="blackOverlay" class="w-full h-full absolute"></span>
         </div>
     </div>
 </template>
