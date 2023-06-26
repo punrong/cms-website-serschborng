9fa6e3e05c9a54ae98fadc1888a5f704592d5d0e
Punrong <punrongrany1999@gmail.com>
2023-06-26 10:10:25 -0400

    fix(Vite): issue with app.css

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
-        }),
+        laravel([
+            'resources/css/app.css',
+            'resources/js/app.js',
+        ]),
         vue({
             template: {
                 transformAssetUrls: {
